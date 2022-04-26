d202739da6662309d64c48c3657d4e3cb5d11fa7
ochuko1996 <georgeochuko7@gmail.com>
2022-04-26 21:09:51 +0100

    till going

--- js/components/video.js
+++ js/components/video.js
@@ -2,15 +2,15 @@ const playBtn = document.getElementById('playBtn')
 const vid = document.querySelector('.vid')
 const overlay = document.querySelector('.overlay-play')
 
-playBtn.addEventListener('click',()=>{
-    vid.play()
-    if(vid.play()){
-        overlay.classList.add('d-none')
-        vid.setAttribute("controls", "controls")
-    }
-})
+    playBtn.addEventListener('click',()=>{
+        vid.play()
+        if(vid.play()){
+            overlay.classList.add('d-none')
+            vid.setAttribute("controls", "controls")
+        }
+    })
 
-vid.addEventListener('ended',(e)=>{
-    vid.removeAttribute("controls")
-    overlay.classList.remove('d-none')
-})
+    vid.addEventListener('ended',(e)=>{
+        vid.removeAttribute("controls")
+        overlay.classList.remove('d-none')
+    })
